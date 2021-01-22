78aae741fa90479f441142ddddce45f37056e945
Cesar <cesinhalius@hotmail.com>
2021-01-22 18:27:08 -0300

    new files about web class

---
+++ exercicos-js/ajax/server.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const multer = require('multer')
+
+
+const app = express()
+
+
+
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+
+
+app.listen(3000, () => console.log('Executando....'))
