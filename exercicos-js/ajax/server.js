21e5cfa6f96676a91b22d7028c02eba32b0f39aa
Cesar <cesinhalius@hotmail.com>
2021-01-26 17:48:43 -0300

    add modified files about modern web class

--- exercicos-js/ajax/server.js
+++ exercicos-js/ajax/server.js
@@ -12,7 +12,22 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+const storege = multer.diskStorage({
+  destination: function (req, file, callback){
+    callback(null, './upload')
+  },
+  filename: function (req,file,callback){
+    callback(null, `${Date.now} ${file.originalname}`)
+  }
+})
 
-
+app.post('/upload', (req,res)=>{
+  upload(req,res,err =>{
+    if(err){
+      return res.end('Ocorreu um erro')
+    }
+    res.end('Concluido com sucesso')
+  })
+})
 
 app.listen(3000, () => console.log('Executando....'))
