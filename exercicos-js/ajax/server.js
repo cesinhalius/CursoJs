6824d4969b60789675b223a77cde139c3a12c144
Cesar <cesinhalius@hotmail.com>
2021-01-26 18:44:17 -0300

    add modified files about modern web class

--- exercicos-js/ajax/server.js
+++ exercicos-js/ajax/server.js
@@ -1,33 +1,36 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const multer = require('multer')
+const bodyParser = require("body-parser");
+const express = require("express");
+const multer = require("multer");
 
+const app = express();
 
-const app = express()
-
-
-
-
-app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const storege = multer.diskStorage({
-  destination: function (req, file, callback){
-    callback(null, './upload')
+  destination: function (req, file, callback) {
+    callback(null, "./upload");
   },
-  filename: function (req,file,callback){
-    callback(null, `${Date.now} ${file.originalname}`)
-  }
-})
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now} ${file.originalname}`);
+  },
+});
 
-app.post('/upload', (req,res)=>{
-  upload(req,res,err =>{
-    if(err){
-      return res.end('Ocorreu um erro')
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end("Ocorreu um erro");
     }
-    res.end('Concluido com sucesso')
+    res.end("Concluido com sucesso");
+  });
+});
+
+app.post('/formulario', (req, res) =>{
+  res.send({
+    ...req.body,
+    id:1
   })
 })
 
-app.listen(3000, () => console.log('Executando....'))
+app.listen(3000, () => console.log("Executando...."));
