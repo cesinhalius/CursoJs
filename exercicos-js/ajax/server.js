9091bdd7400b5d2ee972ec5b8373eddd0a53f837
Cesar <cesinhalius@hotmail.com>
2021-01-27 20:08:46 -0300

    add modified files about modern web class

--- exercicos-js/ajax/server.js
+++ exercicos-js/ajax/server.js
@@ -33,4 +33,12 @@ app.post('/formulario', (req, res) =>{
   })
 })
 
+
+app.get('/parouImpar', (req,res) =>{
+  const par = parseInt(req.query.numero) % 2  === 0
+  res.send({
+    resultado: par ? 'par' : 'impar'
+  })
+})
+
 app.listen(3000, () => console.log("Executando...."));
