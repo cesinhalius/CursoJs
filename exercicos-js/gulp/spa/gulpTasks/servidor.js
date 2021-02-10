8473acd34d01917a9c8e6822da16b53e8789391e
Cesar <cesinhalius@hotmail.com>
2021-02-10 16:11:29 -0300

    new files about web class

---
+++ exercicos-js/gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp");
+
+const webserver = require("gulp-webserver");
+const watch = require("gulp-watch");
+
+function servidor() {
+  return gulp.src("build").pipe(
+    webserver({
+      port: 3000,
+      open: true,
+      livereload: true,
+    })
+  );
+}
+
+function monitorarArquivos(cb) {
+ watch('src/**/*.html', () => gulp.series('appHTML')())
+ watch('src/**/*.scss', () => gulp.series('appCSS')())
+ watch('src/**/*.js', () => gulp.series('appJS')())
+ 
+  return cb();
+}
+
+module.exports = { monitorarArquivos, servidor };
---
+++ exercicos-js/gulp/spa/gulpfile.js
@@ -0,0 +1,18 @@
+const { series, parallel } = require('gulp')
+const gulp = require('gulp')
+
+
+const { appHTML, appCSS, appJS, appIMG } = require('./gulpTasks/app')
+const { depsCSS, depsFONTS } = require('./gulpTasks/app')
+const { monitorarArquivos, servidor } = require('./gulpTasks/servidor')
+
+
+
+module.exports.default = series(
+  parallel(
+    series(appHTML, appCSS, appJS, appIMG),
+    series(depsCSS, depsFONTS)
+  ),
+  servidor,
+  monitorarArquivos
+)
