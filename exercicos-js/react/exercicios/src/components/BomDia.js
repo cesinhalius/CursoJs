119066a453b8761ee3fb96628c3a8b70006a349d
Cesar <cesinhalius@hotmail.com>
2021-04-27 17:24:11 -0300

    add new files about class

---
+++ exercicos-js/react/exercicios/src/components/BomDia.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default props =>
+[
+  <h1 key='k1'>Bom dia {props.nome}</h1>,
+  <h2 key='k2'>Ate breve!!!</h2>,
+  
+
+]
+
+  
---
+++ exercicos-js/react/exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Teste from './components/BomDia';
+
+
+
+ReactDOM.render(<Teste />,document.getElementById('root'));
+
